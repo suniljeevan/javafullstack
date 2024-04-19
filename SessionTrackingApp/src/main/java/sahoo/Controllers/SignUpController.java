c935e90aed63523f0af97e64c1aa1f3762768b37
sunil <36226091+suniljeevan@users.noreply.github.com>
2024-04-19 12:07:14 +0530

    clone it
    extract

---
+++ SessionTrackingApp/src/main/java/sahoo/Controllers/SignUpController.java
@@ -0,0 +1,26 @@
+package sahoo.Controllers;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class SignUpController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	public void doPost(HttpServletRequest req,HttpServletResponse res) {
+		try {
+//code for  receiving and validating
+String un=req.getParameter("u");
+String pwd=req.getParameter("p");
+HttpSession session = req.getSession();
+session.setAttribute("USERNAME",un);
+session.setAttribute("PASSWORD",pwd);
+res.setContentType("text/html");
+res.getWriter().print("<a href="+"login"+">Click for Login"+"</a>");
+		}catch(Exception e) {}
+	}
+}
+
