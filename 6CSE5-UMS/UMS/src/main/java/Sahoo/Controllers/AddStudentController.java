688cd0b3772a9e711424cd09cf7e24afb1f145c5
sunil <36226091+suniljeevan@users.noreply.github.com>
2024-04-20 10:19:22 +0530

    6CSE5-UMS
    UPDATED

--- 6CSE5-UMS/UMS/src/main/java/Sahoo/Controllers/AddStudentController.java
+++ 6CSE5-UMS/UMS/src/main/java/Sahoo/Controllers/AddStudentController.java
@@ -1,30 +1,43 @@
 package Sahoo.Controllers;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-/**
- * Servlet implementation class AddStudentController
- */
 public class AddStudentController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
     public AddStudentController() {
         super();
-        // TODO Auto-generated constructor stub
     }
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection c=DriverManager.getConnection(
+					"jdbc:mysql://localhost:3306/6CSE5","root","sunil");
+			String sql="insert into student values(?,?,?)";
+			PreparedStatement p=c.prepareStatement(sql);
+		int id=Integer.parseInt(request.getParameter("i"));
+		String name=request.getParameter("n");
+		double cgpa=Double.parseDouble(request.getParameter("c"));
+		p.setInt(1, id);
+		p.setString(2, name);
+		p.setDouble(3, cgpa);
+		int r=p.executeUpdate();
+		response.setContentType("text/html");
+		response.getWriter().print("<h2>"+r+" record inserted"+"</h2>");
+		}catch(Exception e) {
+			System.out.println(e);
+		}
 	}
 
 }
+
+
+
+
+
