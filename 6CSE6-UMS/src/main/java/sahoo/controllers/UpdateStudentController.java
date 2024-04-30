27800e2c23dd756e0dc77205c557ce39eee64e41
sunil <36226091+suniljeevan@users.noreply.github.com>
2024-04-30 16:10:13 +0530

    update controller
    check

--- 6CSE6-UMS/src/main/java/sahoo/controllers/UpdateStudentController.java
+++ 6CSE6-UMS/src/main/java/sahoo/controllers/UpdateStudentController.java
@@ -1,30 +1,39 @@
 package sahoo.controllers;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class UpdateStudentController
- */
 public class UpdateStudentController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
     public UpdateStudentController() {
         super();
         // TODO Auto-generated constructor stub
     }
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        try {
+     	Class.forName("com.mysql.jdbc.Driver");
+     	Connection c=DriverManager.getConnection(
+     			"jdbc:mysql://localhost:3306/6cse6","root","sunil");
+     	String rn=request.getParameter("r");
+     	String nm=request.getParameter("n");
+     	double cgpa=Double.parseDouble(request.getParameter("c"));
+     	int sem=Integer.parseInt(request.getParameter("s"));
+     	String sql="update student set name=?, cgpa=?,semester=? where regdno=?";
+     	PreparedStatement p=c.prepareStatement(sql);
+     	p.setString(1, nm);
+     	p.setDouble(2, cgpa);
+     	p.setInt(3, sem);
+     	p.setString(4,rn);
+     	p.executeUpdate();
+     	response.getWriter().println("<h2> record updated");
+        }catch(Exception e) {}
+ 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-	}
-
-}
+ }
