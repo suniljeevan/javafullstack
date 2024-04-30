28567df6e51d92f36fd92929ef9083fcf53c52cc
sunil <36226091+suniljeevan@users.noreply.github.com>
2024-04-30 16:14:07 +0530

    delete student controller
    check

--- 6CSE6-UMS/src/main/java/sahoo/controllers/DeleteStudentController.java
+++ 6CSE6-UMS/src/main/java/sahoo/controllers/DeleteStudentController.java
@@ -1,30 +1,35 @@
 package sahoo.controllers;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-/**
- * Servlet implementation class DeleteStudentController
- */
 public class DeleteStudentController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
     public DeleteStudentController() {
         super();
         // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		try {
+	    	Class.forName("com.mysql.jdbc.Driver");
+	    	Connection c=DriverManager.getConnection(
+	    			"jdbc:mysql://localhost:3306/6cse6","root","sunil");
+	    	String rn=request.getParameter("r");
+	    	String sql="delete from student where id=?";
+	    	PreparedStatement p=c.prepareStatement(sql);
+	    	p.setString(1, rn);
+	    	p.executeUpdate();
+	    	response.getWriter().println("<h2> record deleted");
+	       }catch(Exception e) {}
+		}
+
 	}
 
-}
+
