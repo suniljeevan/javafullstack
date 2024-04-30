20ab6005c44537a20f15228c59184b8777b24bf0
sunil <36226091+suniljeevan@users.noreply.github.com>
2024-04-30 16:02:40 +0530

    AddController
    check

--- 6CSE6-UMS/src/main/java/sahoo/controllers/AddStudentController.java
+++ 6CSE6-UMS/src/main/java/sahoo/controllers/AddStudentController.java
@@ -1,39 +1,41 @@
 package sahoo.controllers;
-
+import java.sql.*;
 import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-/**
- * Servlet implementation class AddStudentController
- */
 public class AddStudentController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
     public AddStudentController() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+       try {
+    	Class.forName("com.mysql.jdbc.Driver");
+    	Connection c=DriverManager.getConnection(
+    			"jdbc:mysql://localhost:3306/6cse6","root","sunil");
+    	String rn=request.getParameter("r");
+    	String nm=request.getParameter("n");
+    	double cgpa=Double.parseDouble(request.getParameter("c"));
+    	int sem=Integer.parseInt(request.getParameter("s"));
+    	String sql="insert into student values(?,?,?,?)";
+    	PreparedStatement p=c.prepareStatement(sql);
+    	p.setString(1, rn);
+    	p.setString(2, nm);
+    	p.setDouble(3, cgpa);
+    	p.setInt(4,sem);
+    	p.executeUpdate();
+    	response.getWriter().println("<h2> record inserted");
+       }catch(Exception e) {}
 	}
 
 }
+
+
+
+
+
+
+
