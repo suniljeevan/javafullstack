e0f94ebc6de233f042e112b9563f344f093a9706
sunil <36226091+suniljeevan@users.noreply.github.com>
2024-04-30 15:43:37 +0530

    6CSE6 UMS
    DONE

---
+++ 6CSE6-UMS/src/main/java/sahoo/controllers/UpdateStudentController.java
@@ -0,0 +1,30 @@
+package sahoo.controllers;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class UpdateStudentController
+ */
+public class UpdateStudentController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public UpdateStudentController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
