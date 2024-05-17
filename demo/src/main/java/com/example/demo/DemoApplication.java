df680bcfb52b084e7f1e51a489affb13b7cafe9d
sunil <36226091+suniljeevan@users.noreply.github.com>
2024-05-17 12:11:54 +0530

    demo check
    done

--- demo/src/main/java/com/example/demo/DemoApplication.java
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -1,9 +1,13 @@
 package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
+@ComponentScan
 public class DemoApplication {
 
 	public static void main(String[] args) {
